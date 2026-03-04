43f0735583ab5f79a3d93b3181aa6272de9f605d
Mahmood Iftee <mahmoodiftee@gmail.com>
2026-03-04 23:51:15 +0600

    Added proper .gitignore

--- apps/payment-service/dist/payment/dto/charge-payment.dto.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ChargePaymentDto = void 0;
-class ChargePaymentDto {
-}
-exports.ChargePaymentDto = ChargePaymentDto;
